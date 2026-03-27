3e44b5faa90a62d0fde9ec99c3dbf5db9193360c
rehe-papp <raidorehepapp@gmail.com>
2026-03-27 13:40:42 +0200

    deploy fix3

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = process.env.EXPORT_STATIC
+const nextConfig: NextConfig = process.env.EXPORT_STATIC === 'true'
   ? {
       output: 'export',
       trailingSlash: true,
       basePath: '/stat-amet-praktika',
+      images: {
+        unoptimized: true, 
+      },
     }
   : {
       /* config options here */
