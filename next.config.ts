84f5fdc104ceaafe39b85bd11e24b38ad38853b4
rehe-papp <raidorehepapp@gmail.com>
2026-03-27 13:17:45 +0200

    deployment fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/stat-amet-praktika',
-};
+const nextConfig: NextConfig = process.env.EXPORT_STATIC
+  ? {
+      output: 'export',
+      trailingSlash: true,
+      basePath: '/stat-amet-praktika',
+    }
+  : {
+      /* config options here */
+    };
 
 export default nextConfig;
--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run build && npm run start',
+    command: 'npm run dev',
     url: 'http://localhost:3000',
     timeout: 120000,
     reuseExistingServer: !process.env.CI,
