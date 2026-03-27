14895d2eb7ad3d51af4618535d806bed7042b159
rehe-papp <raidorehepapp@gmail.com>
2026-03-27 11:45:32 +0200

    deployment test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/stat-amet-praktika',
 };
 
 export default nextConfig;
